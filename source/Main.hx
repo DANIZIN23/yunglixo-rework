58a8e02a5f75e68b37901884471d8239ff6f94f8
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-11-20 14:11:06 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,5 +1,6 @@
 package;
 
+import openfl.utils.Assets;
 import flixel.FlxBasic;
 import flixel.FlxG;
 import flixel.FlxGame;
@@ -28,6 +29,7 @@ import sys.io.File;
 import sys.io.Process;
 
 
+import lime.system.System;
 // Here we actually import the states and metadata, and just the metadata.
 // It's nice to have modularity so that we don't have ALL elements loaded at the same time.
 // at least that's how I think it works. I could be stupid!
@@ -42,6 +44,8 @@ class Main extends Sprite
 
 	public static var gameVersion:String = '0.3.1';
 
+	public static var path:String = System.applicationStorageDirectory;
+	
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var infoCounter:Overlay; // initialize the heads up display that shows information before creating it.
@@ -167,7 +171,7 @@ class Main extends Sprite
 		// addChild(new FPS(10, 3, 0xFFFFFF));
 
 		// begin the discord rich presence
-		#if DISCORD_RPC
+		#if desktop
 		Discord.initializeRPC();
 		Discord.changePresence('');
 		#end
@@ -233,7 +237,7 @@ class Main extends Sprite
 		dateNow = StringTools.replace(dateNow, " ", "_");
 		dateNow = StringTools.replace(dateNow, ":", "'");
 
-		path = "crash/" + "FE_" + dateNow + ".txt";
+		path = Main.path + "crash/" + "FE_" + dateNow + ".txt";
 
 		for (stackItem in callStack)
 		{
@@ -248,8 +252,8 @@ class Main extends Sprite
 
 		errMsg += "\nUncaught Error: " + e.error + "\nPlease report this error to the GitHub page: https://github.com/Yoshubs/Forever-Engine";
 
-		if (!FileSystem.exists("crash/"))
-			FileSystem.createDirectory("crash/");
+		if (!FileSystem.exists(Main.path + "crash/"))
+			FileSystem.createDirectory(Main.path + "crash/");
 
 		File.saveContent(path, errMsg + "\n");
 
