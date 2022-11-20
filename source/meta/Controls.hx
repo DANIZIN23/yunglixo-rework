20ce6db7c82dbc3d49f90b5e17bf17ef41b542b7
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-11-20 17:22:01 -0300

    🤓👍

--- source/meta/Controls.hx
+++ source/meta/Controls.hx
@@ -17,6 +17,13 @@ import flixel.input.gamepad.FlxGamepadButton;
 import flixel.input.gamepad.FlxGamepadInputID;
 
 import flixel.input.keyboard.FlxKey;
+#if android
+import android.flixel.FlxButton;
+import android.flixel.FlxHitbox;
+import android.flixel.FlxVirtualPad;
+#end
+
+
 
 enum abstract Action(String) to String from String
 {
